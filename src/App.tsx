ffa72c62718d42185c236dccfed0885843cb0c87
DanteTerry <sinasa88@gmail.com>
2024-06-22 21:42:54 +0200

    added redux for state management and added persistance api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-
 function App() {
   return (
     <div className="dark">
---
+++ src/app/store.ts
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userSlice from "../features/userSlice.ts";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+//persisting the user state in the local storage
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transform: [saveUserOnlyFilter],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
