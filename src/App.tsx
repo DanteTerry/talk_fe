17a9ca9c6b795b67b71dd20c9aa2cec1f077d103
DanteTerry <sinasa88@gmail.com>
2024-06-22 17:39:10 +0200

    initialize app with vite@react and added tailwind css did color configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FaWhatsapp } from "react-icons/fa";
+function App() {
+  return (
+    <div className="dark">
+      <h1 className="text-2xl text-red-400">Welcome to front end</h1>
+      <FaWhatsapp fill="green" />
+    </div>
+  );
+}
+
+export default App;
