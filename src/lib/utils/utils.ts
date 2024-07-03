0e82c23b75b7efaae0d466045759624650570cef
DanteTerry <sinasa88@gmail.com>
2024-07-03 17:40:08 +0200

    removed cn util function from utils.ts

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,10 +1,5 @@
-import { clsx, type ClassValue } from "clsx";
 import moment from "moment";
-import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
 
 export const trimString = (str: string, length: number = 27) => {
   return str?.length > length ? str?.substring(0, length) + "..." : str;
