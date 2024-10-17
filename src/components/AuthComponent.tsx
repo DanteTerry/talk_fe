5979244b470dd7d1db721f5de5476ee5f35bd56b
DanteTerry <sinasa88@gmail.com>
2024-10-17 14:00:41 +0200

    fixed auth component padding issue

--- src/components/AuthComponent.tsx
+++ src/components/AuthComponent.tsx
@@ -12,7 +12,9 @@ function AuthComponent({
       <div className={`${className} flex h-screen w-full font-inter`}>
         <div className={`bg-fight hidden lg:block lg:w-3/5 ${className}`}></div>
         <div className="flex w-full flex-col items-center justify-center bg-white dark:bg-[#0E1013] lg:w-2/5 lg:rounded-bl-3xl lg:rounded-tl-3xl">
-          <div className="flex max-w-[500px] flex-col gap-2">{children}</div>
+          <div className="flex max-w-[500px] flex-col gap-2 px-6 md:px-0">
+            {children}
+          </div>
         </div>
       </div>
     </div>
